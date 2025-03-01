475706b6a3ddd1bbd626504e27c848587cbed112
ruhanaatiq <ruhana.atiq@gmail.com>
2025-03-01 12:57:17 +0600

    added an blog.html file

--- script.js
+++ script.js
@@ -24,3 +24,17 @@
         document.querySelector('.relative').addEventListener('click', function() {
             document.getElementById('colorPalette').classList.toggle('hidden');
         });
+
+        const today = new Date();
+  
+        // Format the date as "Month DD, YYYY"
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = today.toLocaleDateString('en-US', options);
+      
+        // Display the formatted date in the HTML
+        document.getElementById('current-date').textContent = formattedDate
+
+
+        document.getElementById("clickable-card").addEventListener("click", function () {
+            window.location.href = "blog.html"; // Change this to your desired HTML file
+          });
